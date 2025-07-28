355e020305f517801c54680057830296cd229a63
thiennvt0212 <trungthien0212@gmail.com>
2025-07-28 13:48:26 +0700

    test commit

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -11,7 +11,7 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      đây là nhánh của trung thiện
+      đây là nhánh của trung thiện nè
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
     </h3>
