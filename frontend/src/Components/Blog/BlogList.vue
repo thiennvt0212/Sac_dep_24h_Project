1ed09b33983267c1fbd7bdf0d8a053d0012a9037
Gn0rt <108060689+Gn0rt@users.noreply.github.com>
2025-07-29 15:43:47 +0700

    fake data

--- frontend/src/Components/Blog/BlogList.vue
+++ frontend/src/Components/Blog/BlogList.vue
@@ -1,5 +1,174 @@
 <script setup>
+import { ref, computed } from 'vue';
+import blogImg1 from '../../assets/Image/blogImg1.png';
+import BlogPost from './BlogPost.vue';
+const allPost = ref([
+    {
+        id: 1,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 2,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 3,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 4,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 5,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 6,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 7,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 8,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 9,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 10,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 11,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 12,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 13,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 14,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 15,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 16,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 17,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+    {
+        id: 18,
+        type: 'cbd pastries',
+        title: 'Making CBD-Infused Pastries And Cupcakes',
+        description: 'Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. Pretium lectus quam id...',
+        image: new URL('../../assets/Image/blogImg1.png', import.meta.url).href,
+        date: '10/03/2003'
+    },
+]);
+
+// Reactive data
+const currentPage = ref(1);
+const postsPerPage = ref(6); // 6 bài mỗi trang
+const totalPages = computed(() => Math.ceil(allPost.value.length / postsPerPage.value));
+
+// Tính toán bài viết hiển thị trên trang hiện tại
+const displayedPosts = computed(() => {
+  const startIndex = (currentPage.value - 1) * postsPerPage.value
+  const endIndex = startIndex + postsPerPage.value
+  return allPost.value.slice(startIndex, endIndex)
+})
 </script>
 <template>
-    <h1>Blog List</h1>
+    
+    <div>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap 6">
+            <div v-for="post in displayedPosts" :key="post.id">
+                <blog-post :post="post" />
+            </div>
+        </div>
+    </div>
+    
 </template>
---
+++ frontend/src/Components/Blog/BlogPost.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+defineProps({
+    post: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+<template>
+    <div>
+        <div>
+            <img :src="post.image" alt="">
+            <p>
+                {{ post.date }}
+            </p>
+        </div>
+        <div>
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.description }}</p>
+            <a href="#">Read More</a>
+        </div>
+    </div>
+</template>
