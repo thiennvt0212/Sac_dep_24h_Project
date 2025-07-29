c8b0578f0bb7f39c2c3916b0be8b2954e1522c52
DinhTheDanh <dinhthedanh1@gmail.com>
2025-07-29 14:02:47 +0700

    WIP: Home component before refactor

---
+++ Documents/workspace/vue/Sac_dep_24h_Project-developer/frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import HomeVue from "./Components/Home.vue";
+import Header from "./layouts/Header.vue";
+</script>
+
+<template>
+  <Header> </Header>
+
+  <HomeVue> </HomeVue>
+</template>
+
+<style scoped></style>
---
+++ Documents/workspace/vue/Sac_dep_24h_Project-developer/frontend/src/main.js
@@ -0,0 +1,23 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faCircleDollarToSlot,
+  faChevronRight,
+  faChevronLeft,
+  faChevronDown,
+} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init();
+
+library.add(faCircleDollarToSlot, faChevronRight, faChevronLeft, faChevronDown);
+
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.mount("#app");
