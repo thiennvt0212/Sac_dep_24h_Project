21dc419f92b47c46a8e14b9e5d186cbe881a76fd
kqbang <114908192+HaNgocLanAnh@users.noreply.github.com>
2025-07-30 21:17:12 +0700

    update introduce_page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,14 @@
 
 <script setup>
 import Header from "./layouts/Header.vue";
+import Introduce from "./Pages/Introduce.vue";
 </script>
 
 <template>
   <Header>
 
   </Header>
-
+  <Introduce/>
   <main>
     <div >
       Chào mừng đến với sắc đẹp 24h
