24fb1f6f2c600cc17082c39f633443cbaf7f781e
thiennvt0212 <trungthien0212@gmail.com>
2025-07-31 00:19:11 +0700

    update layout and account page

--- frontend/src/layouts/Header.vue
+++ frontend/src/layouts/Header.vue
@@ -1,28 +1,35 @@
-<script setup></script>
+<script setup>
+  import bgImage from '../assets/Image/homeHeadImg.jpg';
+</script>
 
 <template>
-  <div class="flex flex-row mr-[10px] ml-[10px] h-10 bg-red-500">
+  <!-- <div :style="{ backgroundImage: `url(${bgImage})` }"> -->
+
+  <div class="relative z-11 h-10 w-full">
+  <div class="flex flex-row mr-[10px] ml-[10px] bg-transparent absolute top-0 left-0 right-0 bottom-0 bg-orange-300 " >
     <div class="basis-2/3 flex">
       <div class="flex items-center ml-4 mr-4">
         <font-awesome-icon :icon="['fas', 'headphones']" class="text-[var(--vt-c-white)]" />
-        <p class="pl-2 text-[12px] font-normal text-[var(--vt-c-white)]">(+84) 0313-728-397</p>
+        <p class="pl-2 text-[12px] font-normal text-[var(--vt-c-white)] hover-text-primary cursor-pointer">(+84) 0313-728-397</p>
       </div>
       <div class="flex items-center ml-4 mr-4">
         <font-awesome-icon :icon="['fas', 'envelope']" class="text-[var(--vt-c-white)]"/>
-        <P class="pl-2 text-[12px] font-normal text-[var(--vt-c-white)]">info@themona.global</P>
+        <p class="pl-2 text-[12px] font-normal text-[var(--vt-c-white)] hover-text-primary cursor-pointer">info@themona.global</p>
       </div>
     </div>
     <div class="basis-1/3 flex justify-end" >
-        <div class="flex items-center ml-4 mr-4">
-        <font-awesome-icon :icon="['fas', 'user']"  class="text-[var(--vt-c-white)]"/>
-        <p class="pl-1 pr-1 font-medium text-[var(--vt-c-white)]">ĐĂNG NHẬP</p>
+        <div class="flex items-center ml-4 mr-4 hover-text-primary cursor-pointer">
+        <font-awesome-icon :icon="['fas', 'user']"/>
+        <p class="pl-1 pr-1 font-medium ">ĐĂNG NHẬP</p>
       </div>
-      <div class="flex items-center ml-4 mr-4">
-        <font-awesome-icon :icon="['far', 'heart']"  class="text-[var(--vt-c-white)]"/>
-        <P class="pl-1 pr-1 font-medium text-[var(--vt-c-white)]">YÊU THÍCH</P>
+      <div class="flex items-center ml-4 mr-4 hover-text-primary cursor-pointer">
+        <font-awesome-icon :icon="['far', 'heart']" />
+        <p class="pl-1 pr-1 font-medium">YÊU THÍCH</p>
       </div>
     </div>
   </div>
+  </div>
+  <!-- </div> -->
 </template>
 
 <style></style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,11 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import {faCircleDollarToSlot, faMagnifyingGlass, faHeadphones, faEnvelope, faUser,faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import {faCircleDollarToSlot, faMagnifyingGlass, faHeadphones, faEnvelope, faUser,faCartShopping, faPhoneVolume, faLocationDot, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 import { library } from '@fortawesome/fontawesome-svg-core';
 
-library.add(faCircleDollarToSlot, faMagnifyingGlass, faHeadphones, faEnvelope, faUser, faHeart, faCartShopping)
+library.add(faCircleDollarToSlot, faMagnifyingGlass, faHeadphones, faEnvelope, faUser, faHeart, faCartShopping, faPhoneVolume, faLocationDot, faChevronDown)
 
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
