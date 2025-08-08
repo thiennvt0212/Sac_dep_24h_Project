bb832d7ce42edbec901bfc7c722a28a6539684da
hoangcuong25 <vancuong25112003@gmail.com>
2025-08-08 15:24:45 +0700

    set up BE

---
+++ backend/src/cloudinary/cloudinary.provider.ts
@@ -0,0 +1,16 @@
+// cloudinary.provider.ts
+
+import { v2 as cloudinary } from 'cloudinary';
+import { ConfigService } from '@nestjs/config';
+
+export const CloudinaryProvider = {
+    provide: 'CLOUDINARY',
+    useFactory: (configService: ConfigService) => {
+        return cloudinary.config({
+            cloud_name: configService.get<string>('CLOUDINARY_NAME'),
+            api_key: configService.get<string>('CLOUDINARY_API_KEY'),
+            api_secret: configService.get<string>('CLOUDINARY_SECERT_KEY'),
+        });
+    },
+    inject: [ConfigService],
+};
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT') || 4000;
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true
+  }));
+
+  //config cors
+  app.enableCors(
+    {
+      "origin": true,
+      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "preflightContinue": false,
+      credentials: true
+    }
+  );
+
+  // prefix api/v1
+  app.setGlobalPrefix('api/v1', { exclude: [''] })
+
+  await app.listen(port, '0.0.0.0');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
