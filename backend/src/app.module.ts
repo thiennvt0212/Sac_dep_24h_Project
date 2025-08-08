eec43f770b46e6a8a0f2623d0ce1d1096a972cd1
thiennvt0212 <trungthien0212@gmail.com>
2025-08-08 22:43:00 +0700

    add module card, product, user

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './modules/products/products.module';
+import { UserModule } from './modules/user/user.module';
+import { CartModule } from './modules/cart/cart.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
+    ProductsModule,
+    UserModule,
+    CartModule,
   ],
   controllers: [AppController],
   providers: [AppService],
